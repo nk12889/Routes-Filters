9bcfad4afc76071f5d0f33d0c6ff2a6ea57c5a36
Nitish <nk12889@gmail.com>
2024-03-25 10:54:32 +0530

    routes and filters

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes= require('./routes/shop')
+const errorRoutes= require('./routes/error')
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
+app.use(errorRoutes);
 
 
 app.listen(3000);
